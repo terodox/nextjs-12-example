7dce4d4aaa5807a11be4a1065b95b1f5be3bcc0b
Andrew Desmarais <andy.desmarais@meltwater.com>
2022-10-07 09:35:42 -0400

    Updating to test previews

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          This is a demo of Next js 11 - by Andy
+          This is a demo of Next js 11 - by Andy to Test Previews
         </h1>
 
         <p className={styles.description}>
